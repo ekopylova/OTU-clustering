87e92dc6c89b546df7d5862c56b8536a808a99d1
Jenya Kopylova <jenya.kopylov@gmail.com>
2015-03-18 12:23:17 -0600

    throw comment on non existing file

--- python_scripts/run_summarize_tables.py
+++ python_scripts/run_summarize_tables.py
@@ -84,6 +84,7 @@ if __name__ == '__main__':
                 for study in studies[datatype]:
                     search_dir = os.path.join(rootdir, datatype, method, "%s_%s" % (tool, study))
                     outdir = os.path.join(outdir_root, datatype, method, "%s_%s" % (tool, study))
+                    otu_table = "otu_table_mc2.biom"
                     # check BIOM table exists
                     if not os.path.exists(os.path.join(search_dir, otu_table)):
                         print "file %s does not exist" % os.path.join(search_dir, otu_table)
@@ -91,7 +92,6 @@ if __name__ == '__main__':
                     # compute BIOM table summary if not already done
                     if not os.path.exists(outdir):
                         os.makedirs(outdir)
-                        otu_table = "otu_table_mc2.biom"
 
                         # run BIOM's summarize table command
                         summarize_tbl_command = ["biom",
