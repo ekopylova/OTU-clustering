65eae3edcd1ae3024f4f33beba124114f57ab76a
Jenya Kopylova <jenya.kopylov@gmail.com>
2015-11-11 08:29:30 -0700

    updates to launch scripts

--- shell_scripts/run_micca.sh
+++ shell_scripts/run_micca.sh
@@ -6,7 +6,8 @@ jobs_to_start=$3      # for parallel_assign_taxonomy_rdp.py
 reference_fasta=$4    # for parallel_assign_taxonomy_rdp.py
 reference_taxonomy=$5 # for parallel_assign_taxonomy_rdp.py
 study=$6
-template_str=$8       # PyNAST
+template_str=$7       # PyNAST
+quality=$8
 declare -A micca_trimlen=( ["1688"]="150" ["1686"]="150" ["1685"]="200" ["449"]="200" ["even"]="150" ["staggered"]="150" ["nematodes"]="200" ["632"]="100" ["2107"]="150")
 
 mkdir $output_dir
@@ -14,18 +15,24 @@ mkdir $output_dir
 
 # Filter reads (note: barcodes and primers assumed already removed and
 # sequence IDs in QIIME's format SAMPLENAME_SEQID)
-micca-preproc $reads -q 20 -l ${micca_trimlen["$study"]} -o $output_dir/pre
+micca-preproc $reads -q ${quality} -l ${micca_trimlen["$study"]} -o $output_dir/pre
 
 # Replace QIIME's SAMPLENAME and SEQID delimiter '_' by OTUCLUST's '||'
+# Assumes 4 line FASTQ entry in the format:
+#   @SAMPLENAME_SEQID ...
+#   ACTGGAC ..
+#   +
+#   ghfggih ..
 reads_file=$(basename $reads)
-sed -i 's/_/||/' $output_dir/pre/${reads_file}
+awk -F"[_ ]" 'NR % 2 == 1 { if ($1 ~ /^@/) print $1"||"$2; else print $0;} NR % 2 == 0 {print $0;}' $output_dir/pre/${reads_file} > $output_dir/pre/${reads_file}_2
+mv $output_dir/pre/${reads_file}_2 $output_dir/pre/${reads_file}
 
 # De novo clustering (includes de-replication, sorting by abundance, de novo
 # chimera removal, clustering using a greedy approach)
 reads_name="${reads_file%.*}"
 # Compute number of sequences in FASTQ file, run --derep-fast in OTUCLUST for
 # datasets with 200000+ sequences
-num_lines=$(wc -l $output_dir/pre/$reads | awk '{print $1/4}' | bc)
+num_lines=$(wc -l $output_dir/pre/${reads_file} | awk '{print $1/4}' | bc)
 if [ $num_lines > 200000 ]
 then
     otuclust -f fastq -s 0.97 -c --derep-fast \
@@ -56,7 +63,7 @@ parallel_assign_taxonomy_rdp.py -i $output_dir/${reads_name}_rep.fa -o $output_d
 # Create BIOM table 
 echo "make_otu_table.py"
 make_otu_table.py -i $output_dir/${reads_name}_otus.txt -o $output_dir/otu_table.biom \
--t $output_dir/rdp_assigned_taxonomy/otus_tax_assignments.txt
+-t $output_dir/rdp_assigned_taxonomy/${reads_name}_rep_tax_assignments.txt
 
 # Align sequences command 
 echo "parallel_align_seqs_pynast.py"
